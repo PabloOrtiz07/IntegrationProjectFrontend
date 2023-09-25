edfef82541a618d11b87424bf2462fe5a7eca92b
Pablo Ortiz <63486413+PabloOrtiz07@users.noreply.github.com>
2023-09-25 11:30:43 -0300

    My first commit on this project

---
+++ IntegrationProjectFrontend/IntegrationProjectFrontend/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿
+using Data.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntegrationProjectFrontend.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Ingresar(LoginDto login)
+        {
+            return View("~/Views/Home/Index.cshtml");
+        }
+    }
+}
