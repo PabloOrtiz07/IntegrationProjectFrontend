efe46f7df0422a35e84aded19454e260cfb64f84
Pablo Ortiz <63486413+PabloOrtiz07@users.noreply.github.com>
2023-09-30 11:59:22 -0300

    I just have started my frontend project

--- IntegrationProjectFrontend/Data/DTOs/LoginDto.cs
+++ IntegrationProjectFrontend/Data/DTOs/LoginDto.cs
@@ -8,7 +8,8 @@ namespace Data.DTOs
 {
     public class LoginDto
     {
-        public string Mail { get; set; }
+        public string Email { get; set; }
+
         public string Password { get; set; }
     }
 }
--- IntegrationProjectFrontend/IntegrationProjectFrontend/Controllers/LoginController.cs
+++ IntegrationProjectFrontend/IntegrationProjectFrontend/Controllers/LoginController.cs
@@ -1,19 +1,67 @@
-﻿
-using Data.DTOs;
+﻿using Data.DTOs;
+using Data.Base;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Newtonsoft.Json;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using IntegrationProjectFrontend.Models;
+using IntegrationProjectFrontend.ViewModels;
 
 namespace IntegrationProjectFrontend.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly IHttpClientFactory _httpClient;
+
+        public LoginController(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public IActionResult Login()
         {
             return View();
         }
 
-        public IActionResult Ingresar(LoginDto login)
+        public async Task<IActionResult> LoginByEmail(LoginDto login)
         {
-            return View("~/Views/Home/Index.cshtml");
+            var baseApi = new BaseApi(_httpClient);
+            var token = await baseApi.PostToApi("Authorize", login);
+            var responseLogin = token as OkObjectResult;
+
+            var responseObject = JsonConvert.DeserializeObject<ApiResponse<UserLogin>>(responseLogin.Value.ToString());
+
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+            // Claim claimName = new(ClaimTypes.Name, responseObject.Data.FirstName);
+            Claim claimRole = new(ClaimTypes.Role, "Administrator");
+
+
+           // identity.AddClaim(claimName);
+            identity.AddClaim(claimRole);
+
+            ClaimsPrincipal claimPrincipal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimPrincipal, new AuthenticationProperties
+            {
+                ExpiresUtc = DateTime.Now.AddHours(1),
+            });
+
+             HttpContext.Session.SetString("Token", responseObject.Data.Token);
+
+             var homeViewModel = new HomeViewModel();
+             homeViewModel.Token = responseObject.Data.Token;
+
+            return View("~/Views/Home/Index.cshtml", homeViewModel);
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Login");
         }
     }
 }
